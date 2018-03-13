206a570a8d46f9e90a235662864015d12d96f82d
Nicholas Holley <npholley@gmail.com>
2018-03-13 19:53:10 -0400

    Stuff unborked

--- app/src/main/java/com/nicholasholley/dev/hueboat/ui/MainActivity.kt
+++ app/src/main/java/com/nicholasholley/dev/hueboat/ui/MainActivity.kt
@@ -6,7 +6,6 @@ import android.arch.lifecycle.ViewModelProviders
 import android.content.Context
 import android.os.Bundle
 import android.support.design.widget.BottomNavigationView
-import android.support.design.widget.CollapsingToolbarLayout
 import android.support.v4.app.Fragment
 import android.support.v7.app.AppCompatActivity
 import com.nicholasholley.dev.hueboat.R
@@ -33,31 +32,11 @@ class MainActivity : AppCompatActivity(), HasSupportFragmentInjector, StateChang
 //                vm::class.java
 //        )
 //    }
-    private val navigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
-        when (item.itemId) {
-            R.id.navigation_home -> {
-                message.setText(R.string.title_home)
-                return@OnNavigationItemSelectedListener true
-            }
-            R.id.navigation_dashboard -> {
-                message.setText(R.string.title_dashboard)
-                navigateTo(DiscoveryKey)
-                return@OnNavigationItemSelectedListener true
-            }
-            R.id.navigation_notifications -> {
-                message.setText(R.string.title_settings)
-                return@OnNavigationItemSelectedListener true
-            }
-        }
-        false
-    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setupBackstack(savedInstanceState)
-
-        navigation.setOnNavigationItemSelectedListener(navigationItemSelectedListener)
     }
 
     //HasSupportFragmentInjector
